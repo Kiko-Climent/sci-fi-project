cdfeb372b779f8f13411d238f94dd5d26c7e6ab5
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2023-09-22 11:35:59 +0200

    add new box for final result

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // execute this code once DOM is charged in the document
 document.addEventListener("DOMContentLoaded", function() {
+    
+    // add variable to count the amount of correct answers
+    let selectedOptions = 0;
 
     // get username from URL
     let params = new URLSearchParams(window.location.search);
@@ -9,9 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (userName) {
         // select elements with class "player"
         let playerElements = document.getElementsByClassName("player");
-        let playerElement = playerElements[0];
-        // get username content and set it as player in gamepage
-        playerElement.textContent = `Player: ${userName}`;
+        let playerElementScoreBox = playerElements[0];
+        let playerElementResultBox =playerElements[1];
+        // get username content and set it as player in gamepage both in Scorebox and Resultbox
+        playerElementScoreBox.textContent = `Player: ${userName}`;
+        playerElementResultBox.textContent = `Player: ${userName}`;
         
     }
 
@@ -82,6 +87,9 @@ for (let i = 0; i < checkButtons.length; i++) {
 function incrementScore() {
     let oldscore = document.getElementById("correct").innerText;
     document.getElementById("correct").innerText = ++oldscore;
+
+    // increment the counter of correct answers
+    selectedOptions++;
 }
 
 // add number of incorrect answers
@@ -91,17 +99,18 @@ function incrementIncorrect() {
 }
 
 
+// final result box
+function showFinalResult() {
+    let finalResultElement = document.querySelector('.final-result');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (selectedOptions === 8) {
+        finalResultElement.textContent = 'Your result: You are a master in Sci-Fi';
+    } else if (selectedOptions >= 5 && selectedOptions <= 7) {
+        finalResultElement.textContent = 'Your result: You know a lot about Sci-Fi';
+    } else if (selectedOptions < 5 && selectedOptions > 0) {
+        finalResultElement.textContent = 'Your result: You should watch more Sci-Fi dude.';
+    } else if (selectedOptions === 0) {
+        finalResultElement.textContent = 'Your result: Definitely Sci-Fi is not for you.';
+    }
+}
+showFinalResult();
