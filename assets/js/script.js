986ecc4c389d01555def377004c6112ac2c4c514
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2023-09-22 10:10:23 +0200

    update js with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
-//** add username from log in page to game page as player */
-
+// execute this code once DOM is charged in the document
 document.addEventListener("DOMContentLoaded", function() {
-    alert("May the force be with you!");
+
+    // get username from URL
     let params = new URLSearchParams(window.location.search);
     let userName = params.get('username');
 
+    // once we have the username
     if (userName) {
+        // select elements with class "player"
         let playerElements = document.getElementsByClassName("player");
         let playerElement = playerElements[0];
+        // get username content and set it as player in gamepage
         playerElement.textContent = `Player: ${userName}`;
         
     }
 
     let gameForm = document.getElementById("gameForm");
-
+        // add event once we submit
     gameForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
@@ -21,18 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
         let userName = document.getElementById("user-name").value;
         
 
-        // go to game page
+        // bring username to URL
         window.location.href = `game.html?username=${userName}`;
         
     });
 });
-
-//** mark selected option */
+// mark selected option
 let selectedOption = null;
 
 function selectAnswer(correctAnswer) {
     let clickedOption = event.target;
-
+// If user has already one marked, unmarked the other
     if (selectedOption !== null) {
         selectedOption.classList.remove('selected');
     }
@@ -40,7 +42,7 @@ function selectAnswer(correctAnswer) {
     clickedOption.classList.add('selected');
     selectedOption = clickedOption;
 
-    // Check if the selected option is correct
+    // Check if the selected option is correct in console
     if (clickedOption.innerText === correctAnswer) {
         console.log("That's Correct!");
     } else {
@@ -51,13 +53,14 @@ function selectAnswer(correctAnswer) {
 //** */ add checker correct or incorrect and modify color
 let checkButtons = document.querySelectorAll('[data-type="check-answer"]');
 
+// add click event to each check button
 for (let i = 0; i < checkButtons.length; i++) {
     checkButtons[i].addEventListener('click', function() {
         let quiz = this.parentElement;
         // make correct answer light green once it's checked
         let correctAnswer = quiz.querySelector('.answer-option[data-correct="true"]');
         let selectedAnswer = quiz.querySelector('.answer-option.selected');
-
+        
         if (!selectedAnswer) return;
 
         if (selectedAnswer === correctAnswer) {
@@ -86,6 +89,7 @@ function incrementIncorrect() {
     let oldscore = document.getElementById("incorrect").innerText;
     document.getElementById("incorrect").innerText = ++oldscore;
 }
+
 
 
 
